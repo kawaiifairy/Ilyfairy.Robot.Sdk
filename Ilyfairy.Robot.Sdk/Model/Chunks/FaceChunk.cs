e1cfb80014b02f00b8282aa0cf44f036c0378d1a
小仙女 <me@ilyfairy.com>
2022-03-09 19:50:29 +0800

    支持表情

--- Ilyfairy.Robot.Sdk/Model/Chunks/FaceChunk.cs
+++ Ilyfairy.Robot.Sdk/Model/Chunks/FaceChunk.cs
@@ -3,5 +3,17 @@
     public class FaceChunk : MessageChunk
     {
         public int Face { get; set; }
+        internal FaceChunk()
+        {
+
+        }
+
+        public FaceChunk(int id)
+        {
+            OriginText = $"[CQ:face,id={id}]";
+            this.Type = CQCode.face;
+            this.Face = id;
+        }
+
     }
 }
